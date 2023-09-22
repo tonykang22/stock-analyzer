7de59cd4c168ecdb4c3a36c249e80736f9a1b256
tonykang22 <tony.kang486@gmail.com>
2023-09-22 13:47:33 +0900

    init

---
+++ src/test/java/com/tony/stockanalyzer/StockAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tony.stockanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
